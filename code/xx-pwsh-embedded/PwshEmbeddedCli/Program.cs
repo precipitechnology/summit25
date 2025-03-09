e96fd87d6c85c1dff99fbb04ec852e7f33773612
Ryan Coates <ryan.coates@gmail.com>
2025-03-09 17:59:13 -0600

    simple embedded pwsh example

---
+++ code/xx-pwsh-embedded/PwshEmbeddedCli/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+using (var runspace = RunspaceFactory.CreateRunspace())
+{
+    runspace.Open();
+    using var ps = PowerShell.Create();
+    
+    ps.Runspace = runspace;
+    ps.AddScript("Get-Process | Select Name,Id | ConvertTo-Json");
+
+    var results = ps.Invoke();
+
+    if (results.Count > 0)
+    {
+        Console.WriteLine(results[0].ToString());
+    }
+    else if (ps.HadErrors)
+    {
+        foreach (var error in ps.Streams.Error)
+        {
+            Console.WriteLine($"PowerShell Error: {error}");
+        }
+    }
+}
