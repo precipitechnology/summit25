cc7964f19d6d2bf33664799f8171c4f7ff7526ad
Ryan Coates <ryan.coates@gmail.com>
2025-03-21 07:45:41 -0600

    simple classlib scaffold

---
+++ code/xx-csharp-classlib/ADHealthCheckBuilder.cs
@@ -0,0 +1,29 @@
+﻿// We must define usings for dependencies, such as other libraries
+using System.DirectoryServices.ActiveDirectory;
+using System.Runtime.InteropServices;
+
+// Namespace again (File Scoped)
+namespace SampleLibrary;
+
+public class ADHealthCheckBuilder
+{
+    // Properties of the class defined
+    public Guid Id { get; set; }
+    public DateTimeOffset CreatedAt { get; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public Forest ADForest { get; set; }
+
+    // Constructor (same name as class)
+    public ADHealthCheckBuilder()
+    {
+        // Make sure we don't try run a Windows only API call on a non Windows platform
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            throw new PlatformNotSupportedException("Active Directory health checks are only supported on Windows.");
+        }
+        
+        ADForest = Forest.GetCurrentForest();
+        CreatedAt = DateTimeOffset.UtcNow;
+        UpdatedAt = CreatedAt;
+    }
+}
